c1803de2f1adf62d9a1f2680dab4b69059b4f273
amanrajput2001 <90300203+amanrajput2001@users.noreply.github.com>
2022-01-04 18:03:41 +0530

    Update queue.py

--- core/queue.py
+++ core/queue.py
@@ -58,7 +58,7 @@ class Queue(asyncio.Queue):
         string = ""
         for x, item in enumerate(queue):
             if x < 10:
-                string += f"**{x+1}. [{item.title}]({item.yt_url})** \n- Requested By: {item.requested_by.mention}\n"
+                string += f"**{x+1}. [{item.title}]({item.yt_url})** \n- ミ★𝙍𝙀𝙌𝙐𝙀𝙎𝙏𝙀𝘿 𝘽𝙔★彡: {item.requested_by.mention}\n"
             else:
                 string += f"`\n...{len(queue)-10}`"
                 return string
